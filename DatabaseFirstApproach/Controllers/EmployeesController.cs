223b84447478f858615c579aee183f9fc7e6a5bd
avinashbalajee31 <avinashbalajee03@gmail.com>
2022-07-20 14:31:59 +0530

    Add project files.

---
+++ DatabaseFirstApproach/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseFirstApproach.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseFirstApproach.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : Controller
+    {
+       
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            var employees = new List<Employee>();
+            using (var context = new employeesContext())
+            {
+                employees = context.Employees.ToList();
+            }
+            return employees;
+        }
+
+       
+    }
+}
---
+++ DatabaseFirstApproach/Models/employeesContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirstApproach.Models
+{
+    public partial class employeesContext : DbContext
+    {
+        public employeesContext()
+        {
+        }
+
+        public employeesContext(DbContextOptions<employeesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=avib3131@SQL;database=employees", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("employees");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(45)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
